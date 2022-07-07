cf5189fe66450aa9b8e33440e0ebe2b5f62ab9a9
Erico Pieri <erico.pieri@gmail.com>
2022-07-07 12:18:08 -0300

    init

---
+++ core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from statistics import mode
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+    def __str__(self):
+        return self.descricao
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField()
+
+    def __str__(self):
+        return self.nome
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
