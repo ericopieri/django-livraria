2ac55c2ce1e21b4c340906601c33a6430338e758
Erico Pieri <erico.pieri@gmail.com>
2022-07-08 06:15:16 -0300

    adicao rest_framework ao projeto

---
+++ core/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from rest_framework.serializers import ModelSerializer
+
+from core.models import Categoria
+
+
+class CategoriaSerializer(ModelSerializer):
+    class Meta:
+        model = Categoria,
+        fields = '__all__'
