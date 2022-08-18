e697073bb6f08ee1547600e1871579aa90a86d32
Erico Pieri <erico.pieri@gmail.com>
2022-08-18 12:03:41 -0300

    criação Serializer para Model Editora

--- core/serializers.py
+++ core/serializers.py
@@ -6,3 +6,9 @@ class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
         fields = "__all__"
+
+
+class EditoraSerializer(ModelSerializer):
+    class Meta:
+        model = Editora
+        fields = "__all__"
