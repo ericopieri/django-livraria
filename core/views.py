dd86f8851936b0de6c487d2b0f623d8502fa93f7
Erico Pieri <erico.pieri@gmail.com>
2022-08-18 12:05:23 -0300

    criação View para Model Editora

--- core/views.py
+++ core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
-from core.models import Categoria
-from core.serializers import CategoriaSerializer
+from core.models import Categoria, Editora
+from core.serializers import CategoriaSerializer, EditoraSerializer
 
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
