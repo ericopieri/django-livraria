f3c01814ea7604fcf34dc7999f606e5aba42dd76
Erico Pieri <erico.pieri@gmail.com>
2022-08-18 11:31:57 -0300

    criação e registro da Model Editora

--- core/models.py
+++ core/models.py
@@ -5,3 +5,11 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
