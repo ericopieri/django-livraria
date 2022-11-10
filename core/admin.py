200c9868e7e4038f50107a6df29eaebf1ef4fb42
ericopieri <erico.pieri@gmail.com>
2022-11-09 22:26:44 -0300

    formulario admin para cadastro de Usuarios

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,33 @@
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 
-from core.models import Categoria, Editora, Autor, Livro
+from core.models import Categoria, Editora, Autor, Livro, Usuario
+
+
+class UsuarioAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": (
+            "first_name", "last_name", "email", "cpf", "telefone", "data_nascimento")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Autor)
 admin.site.register(Livro)
+admin.site.register(Usuario, UsuarioAdmin)
