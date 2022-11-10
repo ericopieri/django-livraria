dc4885a9eeab605af80fbd456c8bac48f3fb2a19
ericopieri <erico.pieri@gmail.com>
2022-11-09 22:01:05 -0300

    auterando authentication classes para SimpleJWT

--- livraria/settings.py
+++ livraria/settings.py
@@ -40,6 +40,12 @@ REST_FRAMEWORK = {
     ],
 }
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
