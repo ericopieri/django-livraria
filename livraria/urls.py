71396ec69d600eca678a83f5cf8adb3c22c32607
ericopieri <erico.pieri@gmail.com>
2022-11-09 22:55:14 -0300

    atualizando urls para drf spetacular

--- livraria/urls.py
+++ livraria/urls.py
@@ -8,6 +8,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 from rest_framework.routers import DefaultRouter
 
 from core.views import CategoriaViewSet, EditoraViewSet, LivroViewSet, AutorViewSet
@@ -22,10 +28,21 @@ router.register(r"livros", LivroViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/media/", include(uploader_router.urls)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
 
 urlpatterns += static(settings.MEDIA_ENDPOINT,
