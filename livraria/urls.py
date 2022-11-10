c718478052273fa93c2239965ad683a48cd41bb4
ericopieri <erico.pieri@gmail.com>
2022-11-09 22:00:19 -0300

    rotas para autenticação

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from rest_framework.routers import DefaultRouter
 
 from core.views import CategoriaViewSet, EditoraViewSet, LivroViewSet, AutorViewSet
@@ -14,4 +19,6 @@ router.register(r"livros", LivroViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
